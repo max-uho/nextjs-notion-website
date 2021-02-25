73b7ac74f2c55c438bb6c0c6c42be6fd6136e924
Max Turnheim <maxturnheim@gmail.com>
2021-02-25 16:16:07 +0100

    more hacky footnotes

--- public/footnotes.js
+++ public/footnotes.js
@@ -11,18 +11,21 @@ while( curMatch = rxp.exec( str ) ) {
 
 str = "a [^first footnote] with [not a footnote] [^second footnote] braces";
 
-//$('.notion-page').html($('.notion-page').html().replace(/\[\^.*?\]/g, '<i></i>'));
-//console.log( $('.notion-page').html() );
 
-
+// VERY HACKY FOOTNOTES
 
 window.onload = function() {
     np = $('.notion-page').first();
-
     str = np.html();
-
-    str = str.replace(/\[\^.*?\]/g, '');
-
+    
+    //str = str.replace(/\[\^.*?\]/g, '<sup id="fnref:foobar"><a href="#fn:foobar">1</a></sup>');
+    var f_number = 0;
+    str = str.replace(/\[\^.*?\]/g, function() {
+        ++f_number;
+        fs_number = f_number.toString();
+        return '<sup id="fnref:'+fs_number+'"><a href="#fn:'+fs_number+'">'+ fs_number +'</a></sup>';
+    });
+        
     np.html(str);
 };
 
