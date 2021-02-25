d9294d66e97e4caa6ad9a5defe4ff2ea525405af
Max Turnheim <maxturnheim@gmail.com>
2021-02-25 17:19:55 +0100

    extremly  hacky footnotes

--- public/footnotes.js
+++ public/footnotes.js
@@ -1,35 +1,55 @@
-var found = [],
-rxp = /\[\^.*?\]/g,
-str = "a [^first footnote] with [not a footnote] [^second footnote] braces",
-curMatch;
-
-while( curMatch = rxp.exec( str ) ) {
-    found.push( curMatch[1] );
-}
-
-//yourTextArea.value = yourTextArea.value.replace(/\$\$(.+?)\$\$/, '<i>$1</i>')
-
-str = "a [^first footnote] with [not a footnote] [^second footnote] braces";
-
 
 // VERY HACKY FOOTNOTES
 
 window.onload = function() {
-    np = $('.notion-page').first();
-    str = np.html();
-    
-    //str = str.replace(/\[\^.*?\]/g, '<sup id="fnref:foobar"><a href="#fn:foobar">1</a></sup>');
+
+
+    var np = $('.notion-page').first();
+    var found = [],
+    rxp = /\[\^.*?\]/g,
+    str = np.html(),
+    curMatch;
+
+    while( curMatch = rxp.exec( str ) ) {
+        found.push(curMatch[0]);
+    }
+
+    console.log(found)
+
     var f_number = 0;
-    str = str.replace(/\[\^.*?\]/g, function() {
+
+    str = str.replace(rxp, function() {
         ++f_number;
         fs_number = f_number.toString();
-        return '<sup id="fnref:'+fs_number+'"><a href="#fn:'+fs_number+'">'+ fs_number +'</a></sup>';
+        return '<sup id="fnref:'+fs_number+'"><a href="#fn:'+fs_number+'" class="footnote-ref" role="doc-noteref">'+ fs_number +'</a></sup>';
     });
-        
+
     np.html(str);
+
+    $(".notion-page-content").append( "<section class=footnotes role=doc-endnotes>\
+        <hr>\
+        <ol>\
+        " );
+
+    var arrayLength = found.length;
+    for (var i = 0; i < arrayLength; i++) {
+        var clean_note = found[i].slice(2,-1);
+        f_note_content = '\
+                <li id=fn:'+(i+1).toString()+' role=doc-endnote>\
+                    <p>\
+                        '+clean_note+'\
+                        <a href=#fnref:'+(i+1).toString()+' class=footnote-backref role=doc-backlink>&#8617;&#xfe0e;</a>\
+                    </p>\
+                </li>\
+        ';
+
+
+        $(".notion-page-content").append(f_note_content);
+        //Do something
+    }
+
+    $(".notion-page-content").append( "</ol>\
+          </section>\
+        " );
+
 };
-
-
-//console.log( $('.notion-page').text());
-
-
